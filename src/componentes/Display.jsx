f82d209eb2e4afe954931b7a629c72af104a6688
natigalo <natigalo1@gmail.com>
2023-06-07 07:13:17 -0500

    display and singin

--- src/componentes/Display.jsx
+++ src/componentes/Display.jsx
@@ -1,22 +1,24 @@
 import React from "react"
 import photo from "/assets/images/logoUser.svg"
+import x from "/assets/images/x.svg"
 export default function Display({ show, setShow, options }) {
     return (
-        <div className="absolute z-20 top-0 left-0 w-[300px] font-inter text-center text-white bg-indigo-700 h-full ">
-            <div className="flex justify-around pt-5" >
+        <div className="absolute z-20 top-0 left-0 w-full lg:w-[320px] font-inter text-center  bg-indigo-700 h-full ">
+            <div className="flex justify-around pt-6 px-[1%]" >
                 <img src={photo} alt="" />
                 <div className="text-white text-left ">
                     <h1 className="font-semibold text-base">Lucas Ezequiel Silva</h1>
-                    <h3 className="font-medium text-xs" >ashfaksayem@gmail.com</h3>
+                    <h3 className="font-medium text-xs text-[#D1D3D4]" >ashfaksayem@gmail.com</h3>
                 </div>
-                <h1 className="cursor-pointer text-sm" onClick={() => setShow(!show)} >X</h1>
+                <img src={x} alt="x" className="cursor-pointer text-sm" onClick={() => setShow(!show)}/>
+                
             </div>
-            <div className="flex text-left justify-between flex-col  h-80 mt-10 pl-5">
-            <div className="hover:bg-white hover:text-indigo-500">Home</div>
-            <div>Mangas</div>
-            <div>My Mangas</div>
-            <div>Favorites</div>
-            <div>Logout</div>
+            <div className="flex text-left justify-between flex-col text-white h-[200px] mt-6 px-[10%] lg:px-5">
+            <div className="hover:bg-white hover:text-indigo-500 rounded-md mx-2 p-1 items-center text-md">Home</div>
+            <div className="hover:bg-white hover:text-indigo-500 rounded-md mx-2 p-1 items-center text-md">Mangas</div>
+            <div className="hover:bg-white hover:text-indigo-500 rounded-md mx-2 p-1 items-center text-md">My Mangas</div>
+            <div className="hover:bg-white hover:text-indigo-500 rounded-md mx-2 p-1 items-center text-md">Favorites</div>
+            <div className="hover:bg-white hover:text-indigo-500 rounded-md mx-2 p-1 items-center text-md">Logout</div>
             </div>
             
         </div>
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,10 @@
+import logom from "/assets/images/logoFooter.svg"
+import asideimg from "/assets/images/asideWelcome.svg"
 export default function SignIn({changeView, setChangeView}) {
     return(
         <main className="flex flex-col pt-20 items-center justify-center">
+            <img src={asideimg} alt="" />
+            <img src={logom} alt="logo" />
             <p>Welcome Back!</p>
             <p>discover</p>
             <h1 onClick={()=>setChangeView(!changeView)} >SINGIN</h1>
